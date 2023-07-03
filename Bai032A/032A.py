5ae8978df23347d3a3a781dbce28c1a277f206bf
21522683 <21522683@gm.uit.edu.vn>
2023-07-03 16:47:31 +0700

    .

---
+++ Bai032A/032A.py
@@ -0,0 +1,14 @@
+﻿
+def main():
+     print('Phan Trọng Tính')
+     print('Bai 032A')
+     n = int (input("Nhap so n: "))
+     s = 0
+     i = 1
+     while i <= n:
+        s = s + 1 / i*(i+1)
+        i = i + 1
+     print('Tong la: ', s)
+     
+if __name__ == "__main__":
+    main()

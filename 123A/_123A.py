61573505375e33eadd6d549275ed41dbe9b0da0b
Kiệt Trần Quang Anh <22520725@ms.uit.edu.vn>
2023-07-03 17:03:50 +0700

    22520725

---
+++ 123A/_123A.py
@@ -0,0 +1,15 @@
+print("Tran Quang Anh Kiet")
+n = int(input("Nhap n: "))
+at = 2
+bt = 1
+ahh = 0
+bhh = 0
+for i in range (2,n+1):
+    ahh = 3*bt+2*at
+    bhh = at + 3*bt
+    at = ahh
+    bt = bhh
+print("a(",n,")= ", ahh, sep='')
+print("b(",n,")= ",bhh , sep ='')
+
+

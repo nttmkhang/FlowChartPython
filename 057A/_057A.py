ededdb9359b4479c925e5236e1bd10e24ce41cb0
BinhUIT <22520125@gm.uit.edu.vn>
2023-07-03 16:20:54 +0700

    BinhUIT

--- 057A/_057A.py
+++ 057A/_057A.py
@@ -1,1 +1,10 @@
-
+n=int(input("Nhap n: ")) 
+i=1 
+s=0 
+while(i<n): 
+    if(n%i==0):
+        s=s+i 
+    i=i+1 
+print("Ten: Dang Le Binh")
+print("Bai 057")
+print("Tong cac uoc so nho hon n la", s)
--- 057B/_057B.py
+++ 057B/_057B.py
@@ -1,1 +1,2 @@
-
+def Nhap():
+    n=int(input("Nhap "))

a26fba800a43d41a9d0784ed273902499c8ece40
gizzly1010 <trantanyo@gmail.com>
2023-07-03 16:29:49 +0700

    22521312

---
+++ 103B/_103B.py
@@ -0,0 +1,19 @@
+print("Tran Nhat Tan")
+
+def TinhToan():
+    s=0
+    e=1
+    i=1
+    while e>=10**-6:
+        e=1/i
+        s+=e
+        i+=2
+    return s
+
+def main():
+    kq=TinhToan()
+    print("\nS(n) = ",round(kq,6),".\n")
+
+if __name__=="__main__":
+    main()
+
---
+++ 104B/_104B.py
@@ -0,0 +1,18 @@
+print("Tran Nhat Tan")
+
+def TinhToan():
+    s=0
+    e=0.5
+    i=1
+    while e>=10**-6:
+        e=1/(i*(i+1))
+        s+=e
+        i+=1
+    return s
+
+def main():
+    kq=TinhToan()
+    print("\nS(n) = ",round(kq,6),".\n")
+
+if __name__=="__main__":
+    main()

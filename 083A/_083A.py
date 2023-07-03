093ea8f9c6fde0bd8533691e971e0ce97d970249
ongtrandong2 <ongtrandong2@gmail.com>
2023-07-03 16:47:31 +0700

    21521957

--- 083A/_083A.py
+++ 083A/_083A.py
@@ -3,13 +3,12 @@ from math import sin
 
 x = int(input("Nhap x:"))
 n = int(input("Nhap n:"))
-s=0
-t=1
-i=1
-while i<=n:
-    t *=x
+s = 0
+t = 1
+i = 1
+while i <= n:
+    t *= x
     s += sin(t)
-    i+=1
+    i += 1
 print("Bai 83A: Tran Dong Dong")
-print("S=",s)
-
+print("S=", s)

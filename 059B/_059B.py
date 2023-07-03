937f1c06daa416bf9bc2b68cf5033460d32e9cee
BinhUIT <22520125@gm.uit.edu.vn>
2023-07-03 18:33:15 +0700

    22520334

--- 059B/_059B.py
+++ 059B/_059B.py
@@ -1,1 +1,19 @@
+﻿def Nhap(): 
+    n=int(input("Nhap n: ")) 
+    return n 
+def DemChuSo(n): 
+    dem=0 
+    t=n 
+    while(t!=0): 
+        dem=dem+1 
+        t=int(t/10) 
+    return dem 
+def main():
+    print("Tên:Nguyễn Tấn Duy")
+    print("Bài 059B") 
+    n=Nhap() 
+    print("Số lượng chữ số của n là: ", DemChuSo(n)) 
+if __name__=="__main__": 
+    main()
 
+    
--- 060_A/_060_A.py
+++ 060_A/_060_A.py
@@ -1,1 +1,9 @@
-
+print("Nguyen Tan Duy")
+n=int(input("Nhap n: "))
+tich=1
+t=n
+while t!=0:
+dv=t%10
+tich=tich*dv
+t=int(t/10)
+print("Tich cac chu so cua",n,"la: ",tich)
